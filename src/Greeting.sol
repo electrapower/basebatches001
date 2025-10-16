c0eea04f0cac48b45a4db5d8a22f5a3f2280e65d
electra <applehoob@gmail.com>
2025-10-16 22:59:56 +0200

    newsubm

--- src/Greeting.sol
+++ src/Greeting.sol
@@ -8,9 +8,11 @@ contract Greeter {
     constructor(string memory _initialGreeting) {
         greeting = _initialGreeting;
     }
+    
     function setGreeting(string memory _newGreeting) public {
         greeting = _newGreeting;
     }
+
     function greet() public view returns (string memory) {
         return greeting;
     }
